761bfaba23a9d61a468c74293dd5c807ba254dae
minkyeongJ <jomk2221@naver.com>
2022-10-19 23:25:03 +0900

    Feat: 크립토 체인져 price chart 변환

--- my-app/src/routes/Coin.tsx
+++ my-app/src/routes/Coin.tsx
@@ -2,7 +2,7 @@ import { useParams, useLocation } from "react-router";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link, useMatch } from "react-router-dom";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -49,6 +49,27 @@ const Description = styled.p`
   margin: 20px 0px;
 `;
 
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
+`;
 interface RouteState {
   name: string;
 }
@@ -116,6 +137,8 @@ export function Coin() {
   const state = location.state as RouteState;
   const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo] = useState<PriceData>();
+  const priceMatch = useMatch("/:coinId/price");
+  const chartMatch = useMatch("/:coinId/chart");
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -165,6 +188,14 @@ export function Coin() {
               <span>{priceInfo?.max_supply}</span>
             </OverviewItem>
           </Overview>
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`price`}>Price</Link>
+            </Tab>
+          </Tabs>
           <Outlet />
         </>
       )}
